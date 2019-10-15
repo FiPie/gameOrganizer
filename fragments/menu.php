88924b555574e83da85b92dc07be2dda7dc5e1e2
Filip Pie <filipiusz@gmail.com>
2019-10-15 16:10:28 +0200

    initial commit

---
+++ fragments/menu.php
@@ -0,0 +1,11 @@
+<nav>
+  <a href="index.php"></a>
+  <a href="timetable.php"></a>
+  <a href="payment"></a>
+  <a href="attendance"></a>
+  <a href="team"></a>
+  <a href="scores"></a>
+  <a href="legalNote"></a>
+  <a href="login"></a>
+  <a href="signIn"></a>
+</nav>
