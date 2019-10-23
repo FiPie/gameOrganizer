a6fb7e7b874be59fbb855aedf8455c68b63200f7
Filip Pie <filipiusz@gmail.com>
2019-10-23 19:22:03 +0200

    little js fix

--- js/gameScript.js
+++ js/gameScript.js
@@ -2,10 +2,12 @@ $(function () {
 
 
     $("nav a").mouseover(function () {
+        $("nav a").finish();
         $(this).animate({left: '50px', right: '50px'}, 50);
         $(this).animate({fontSize: '2em'}, 50);
     });
     $("nav a").mouseleave(function () {
+        $("nav a").finish();
         $(this).animate({left: '50px', right: '50px'}, 250);
         $(this).animate({fontSize: '1em'}, 250);
     });
